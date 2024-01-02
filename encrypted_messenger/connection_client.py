266d353f4a6e20b56910d390c535d5d3cde47bf0
fafa194 <fafa194@hotmail.com>
2024-01-02 23:22:32 +0100

    - Code shock after a christmas-new year break - It has been revised and performed few modifications - Implemented logging - Decryption on both sites is still not available.

--- encrypted_messenger/connection_client.py
+++ encrypted_messenger/connection_client.py
@@ -1,8 +1,8 @@
-import datetime, rsa, socket
+import datetime, rsa, socket, logging
 import json
 from json import dumps
 
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 HOST = socket.gethostname()  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -14,7 +14,6 @@ print(pubKey_export)
 
 message = "lolol"
 
-
 ## Managing the messenger.
 messages = []
 date_time_now = datetime.datetime.now()
@@ -22,53 +21,61 @@ date_time_now_timestamp = date_time_now.strftime("%H:%M")
 
 
 def encryptMessage(text):
-    encryptedMessage = rsa.encrypt(message.encode(),pubKey)
+    encryptedMessage = rsa.encrypt(message.encode(), pubKey)
     return encryptedMessage
 
+
 def decryptMessage(encryptMessage):
-    decryptedMessage = rsa.decrypt(encryptMessage,privKey).decode()
+    decryptedMessage = rsa.decrypt(encryptMessage, privKey).decode()
     return decryptedMessage
 
-def createJsonDumpedMessage(sender,message,timestamp):
+
+def createJsonDumpedMessage(sender, message, timestamp):
     message_detail = {
-        "Sender" : sender,
-        "Message" : message,
-        "Timestamp" : timestamp
+        "Sender": sender,
+        "Message": message,
+        "Timestamp": timestamp
     }
     json_dumped_message = dumps(message_detail)
     return json_dumped_message
 
+
 message_encrypted = encryptMessage(createJsonDumpedMessage)
+json_deserilised = json.loads(createJsonDumpedMessage("X", "22", "10"))
+print(decryptMessage(message_encrypted))
 
-json_deserilised = json.loads(createJsonDumpedMessage("X","22","10"))
 
-
-## Exchange the RSA
-def exchangePubRSA():
+def exchangePubRSA():  # Function that exchanges the RSA key with server
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST,PORT))  # Establish connection
-            s.sendall(pubKey_export) # Export the public key
+            s.connect((HOST, PORT))  # Establish connection
+            s.sendall(pubKey_export)  # Export the public key
 
-            print("Waiting for response")
-            rsa_pubKey_import = s.recv(1024)
-            print(rsa_pubKey_import,type(rsa_pubKey_import))
+            logging.info("Waiting for response")
+            rsa_pubkey_import = s.recv(1024)
+            logging.info(f"Got the key! {rsa_pubkey_import}")
     except Exception as ex:
         print(f"Error on exchange: {ex}")
 
 
-### Client main messeges socket
 exchangePubRSA()
-PORT = 65431
+
+# After exchanging RSA, the messenger establishes a new connection and starts.
 try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Configuring multi-usage of socket
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.connect((HOST, PORT))
         while True:
-            message = input("Message:")
-            s.sendall(message_encrypted)
-            data = s.recv(1024)
-    print(f"Received {data!r}")
+            try:
+                # Sending the encrypted message to server.
+                s.sendall(message_encrypted)
+
+                # Waiting for data from server
+                data = s.recv(1024)
+                logging.info(f"Received: {data.decode()}")
+            except Exception as messageError:
+                logging.error(f"Message error: {messageError}")
+                break
 except Exception as e:
-    print(f"An error occurred: {e}")
-finally:
-    s.close()
+    logging.error(f"Connection error: {e}")
--- encrypted_messenger/connection_server.py
+++ encrypted_messenger/connection_server.py
@@ -1,62 +1,71 @@
 # echo-server.py
 
-import socket, rsa
+import socket, rsa, logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 HOST = socket.gethostname()  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 ## RSA key
-pubKey, privKey = rsa.newkeys(512)
-pubKey_export = pubKey.save_pkcs1(format="DER")
+RSApubKey, RSAprivKey = rsa.newkeys(512)
+pubKey_export = RSApubKey.save_pkcs1(format="DER")
+
 
 ## Decryption process
 def decryptMessage(encryptMessage):
-    decryptedMessage = rsa.decrypt(encryptMessage,privKey).decode()
+    decryptedMessage = rsa.decrypt(encryptMessage, RSAprivKey).decode()
     return decryptedMessage
 
+
 def encryptMessage(text):
-    encryptedMessage = rsa.encrypt(message.encode(),pubKey)
+    encryptedMessage = rsa.encrypt(text.encode(), RSApubKey)
     return encryptedMessage
+
 
 def exchangePubRSA():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST,PORT))
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Configuration allowing multi usage
+            s.bind((HOST, PORT))
             s.listen()
-            print("Waiting for RSA exchange")
+            logging.info(f"Waiting for RSA exchange {pubKey_export}")
             conn, addr = s.accept()
             with conn:
                 conn.sendall(pubKey_export)
-                rsa_pubKey_import = conn.recv(1024)
-                print("SUCCESS", rsa_pubKey_import)
+                rsa_pubkey_import = conn.recv(1024)
+                logging.info(f"Key exchanged! {rsa_pubkey_import}")
     except Exception as ex:
-        print(f"Error on exchange: {ex}")
+        logging.info(f"Error on exchange: {ex}")
     finally:
         s.close()
 
+
 exchangePubRSA()
-PORT = 65431
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Server started. Waiting for connection...")
+try:  # After exchanging RSA, socket starts
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen()
 
-    # Get the pub key from client
-    conn, addr = s.accept()
-    client_rsa_pubKey = conn.recv(1024)
-
-
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    while True:
-        with conn:
-            print(f"Message from {addr[0]}")
-            print(client_rsa_pubKey)
+        # Server is listening for message from client
+        logging.info("Server started. Waiting for message...")
+        conn, addr = s.accept()
+        with conn:  # Once the connection is accepted
+            logging.info(f"Connected by {addr}")
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(1024)  # Data received from client
                 if not data:
                     break
-                conn.sendall(data)
-                print(data)
+                logging.info(f"Received data: {data}")
+                print(type(data))
+                try:
+                    receivedEncrypted = decryptMessage(data)  # Attempt to decrypt the message
+                    print(receivedEncrypted)
+                except Exception as decryptError:
+                    logging.error(f"Error decrypting data: {decryptError}")
+                    break
+except Exception as exi:
+    logging.exception(f"Error: {exi}")
+    s.close()
